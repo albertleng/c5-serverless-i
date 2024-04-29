d90346ef4edbc258ec35e0ee1f8ded9a7c359c8f
albert.leng <albert.leng@acronis.com>
2024-04-29 22:03:54 +0800

    Added AWS SDK and S3 bucket creation in postHandlerTwo function

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
 module.exports.handlerTwo = async (event) => {
     console.log('GET method')
     console.log(event)
@@ -17,12 +20,25 @@ module.exports.handlerTwo = async (event) => {
 module.exports.postHandlerTwo = async (event) => {
     console.log('POST method')
     console.log(event)
+
     // Extract the body from the event
     let body;
     try {
         body = JSON.parse(event.body);
     } catch (error) {
         console.error('Failed to parse JSON: ', error)
+    }
+
+    // Create a new S3 bucket
+    const bucketParams = {
+        Bucket: 'MyS3Bucket', // Replace with your desired bucket name
+    };
+
+    try {
+        const data = await s3.createBucket(bucketParams).promise();
+        console.log('Bucket created successfully', data.Location);
+    } catch (error) {
+        console.error('Failed to create bucket: ', error);
     }
 
     // Do something with the body
